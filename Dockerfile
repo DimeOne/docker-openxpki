658418c37c2d97a304322333604b4d7d606db6d6
Dominic S <git@dominic86.de>
2017-06-01 02:23:50 +0200

    initialc ommit

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+﻿FROM debian:jessie
+
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+ENV S6_OVERLAY_VERSION v1.11.0.1
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y wget && \
+    wget http://packages.openxpki.org/debian/Release.key -O - | apt-key add - && \
+    echo "deb http://packages.openxpki.org/debian/ jessie release" > /etc/apt/sources.list.d/openxpki.list && \
+    echo "deb http://httpredir.debian.org/debian jessie non-free" >> /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get install -y locales && \
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && \
+    locale-gen && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+      libdbd-mysql-perl \
+      libapache2-mod-fcgid \
+      libopenxpki-perl \
+      openxpki-i18n \
+      openca-tools \
+      mysql-client && \
+    a2enmod fcgid && \
+    wget -q -O - https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-amd64.tar.gz | tar xzf - -C / && \
+    apt-get remove -y wget && \
+    apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+ADD scripts/docker-entrypoint.sh /
+RUN chmod 755 /docker-entrypoint.sh
+
+ADD configs/services.d /etc/
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,135 @@
+﻿#
+
+function checkDbVariables {
+  if [ -z "${APP_DB_NAME}" ]; then echo "Missing APP_DB_NAME, set this variable or link a mysql server with the name mysql."; exit 101; fi
+  if [ -z "${APP_DB_HOST}" ]; then echo "Missing APP_DB_HOST, set this variable or link a mysql server with the name mysql."; exit 102; fi
+  if [ -z "${APP_DB_PORT}" ]; then echo "Missing APP_DB_PORT, set this variable or link a mysql server with the name mysql."; exit 103; fi
+  if [ -z "${APP_DB_USER}" ]; then echo "Missing APP_DB_USER, set this variable or link a mysql server with the name mysql."; exit 104; fi
+  if [ -z "${APP_DB_PASS}" ]; then echo "Missing APP_DB_PASS, set this variable or link a mysql server with the name mysql."; exit 105; fi
+}
+
+function waitForDbConnection {
+
+  connectionAttempt=1
+  while ! echo "SHOW GRANTS FOR CURRENT_USER;" | mysql -u ${APP_DB_USER} -p${APP_DB_PASS} -D ${APP_DB_NAME} -h ${APP_DB_HOST} -P ${APP_DB_PORT}; do
+    if [ $connectionAttempt -gt 30 ]; then
+      echo "Maximum amounts of attempts reached, stopping now."
+      exit 1
+    fi
+    echo "Connection to MySql server failed, waiting 5s before trying again. [$connectionAttempt}/30]"
+    sleep 5
+    connectionAttempts=$[$connectionAttempt +1]
+  done
+  echo "Connection to MySql server was successful."
+
+}
+
+function create_config {
+
+  # Use custom configuration if available - otherwise the default
+  if [ -f "/etc/openxpki/createconfig.sh" ]; then
+     echo "Found custom configuration, securing and executing it."
+     chown root:root /etc/openxpki/createconfig.sh
+     chmod 700 /etc/openxpki/createconfig.sh
+     /etc/openxpki/createconfig.sh
+  elif [ -f "/usr/share/doc/libopenxpki-perl/examples/" ]
+    echo "Found no custom createconfig.sh - using default sampleconfig.sh from /usr/share/doc/libopenxpki-perl/examples/sampleconfig.sh"
+    /usr/share/doc/libopenxpki-perl/examples/sampleconfig.sh
+
+}
+
+function init_db {
+
+  # Extract sql file and install database shema
+  zcat /usr/share/doc/libopenxpki-perl/examples/schema-mysql.sql.gz | \
+    mysql -u ${APP_DB_USER} -p${APP_DB_PASS} -D ${APP_DB_NAME} -h ${APP_DB_HOST} -P ${APP_DB_PORT}
+
+}
+
+# Check for linked MYSQL container
+if [ -n "${MYSQL_NAME}" ]; then
+  echo "Found linked MySql container, updating variables."
+    MYSQL_DB_HOST="mysql"
+    MYSQL_DB_NAME=${MYSQL_ENV_MYSQL_DATABASE}
+    MYSQL_DB_PORT=${MYSQL_PORT_3306_TCP_PORT}
+    MYSQL_DB_USER=${MYSQL_ENV_MYSQL_USER}
+    MYSQL_DB_PASS=${MYSQL_ENV_MYSQL_PASSWORD}
+  fi
+
+  # Unset the original variables to prevent leakage
+  unset MYSQL_ENV_MYSQL_PASSWORD MYSQL_ENV_MYSQL_ROOT_PASSWORD
+fi
+
+# Using environment variables that may have been set
+APP_DB_NAME=${APP_DB_NAME:-$MYSQL_DB_NAME}
+APP_DB_HOST=${APP_DB_HOST:-$MYSQL_DB_HOST}
+APP_DB_PORT=${APP_DB_PORT:-$MYSQL_DB_PORT}
+APP_DB_USER=${APP_DB_USER:-$MYSQL_DB_USER}
+APP_DB_PASS=${APP_DB_PASS:-$MYSQL_DB_PASS}
+
+# Create default config files if missing - because of volume
+if [ ! -d /etc/openxpki/config.d ]; then
+  echo "Found no config.d folder in /etc/openxpki - extracting example configuration."
+  tar xzf /usr/share/doc/libopenxpki-perl/examples/openxpki-etc.tgz -C /etc
+fi
+
+echo "Updating database.yml"
+if [ "${APP_DB_NAME}" ]; then echo "Replacing DB_NAME with given APP_DB_NAME: ${APP_DB_NAME}"; sed -i 's/name: .*/name: ${APP_DB_NAME}/' /etc/openxpki/config.d/system/database.yaml; fi
+if [ "${APP_DB_HOST}" ]; then echo "Replacing DB_HOST with given APP_DB_HOST: ${APP_DB_HOST}"; sed -i 's/host: .*/host: ${APP_DB_HOST}/' /etc/openxpki/config.d/system/database.yaml; fi
+if [ "${APP_DB_PORT}" ]; then echo "Replacing DB_PORT with given APP_DB_PORT: ${APP_DB_PORT}"; sed -i 's/port: .*/port: ${APP_DB_PORT}/' /etc/openxpki/config.d/system/database.yaml; fi
+if [ "${APP_DB_USER}" ]; then echo "Replacing DB_USER with given APP_DB_USER: ${APP_DB_USER}"; sed -i 's/user: .*/user: ${APP_DB_USER}/' /etc/openxpki/config.d/system/database.yaml; fi
+if [ "${APP_DB_PASS}" ]; then echo "Replacing DB_PASS with given APP_DB_PASS: ${APP_DB_PASS}"; sed -i 's/passwd: .*/passwd: ${APP_DB_PASS}/' /etc/openxpki/config.d/system/database.yaml; fi
+
+
+# Start depending on parameters
+if [ "$1" == "initdb" ]; then
+  echo "================================================"
+  echo "Received initdb parameter, initiating database."
+  echo "================================================"
+  checkDbVariables
+  waitForDbConnection
+  init_db
+elif [ "$1" == "create_certs"]; then
+  echo "================================================"
+  echo "Received create_certs parameter, creating certificates."
+  echo "================================================"
+  create_config
+  fi
+elif [ "$1" == "wait_for_db" ]; then
+  echo "================================================"
+  echo "Received wait_for_db parameter, Waiting for successful database connection."
+  echo "================================================"
+  checkDbVariables
+  waitForDbConnection
+elif [ "$1" == "run" ]; then
+  waitForDbConnection
+  echo "Launching S6 supervisor to start Apache2 and OpenXPKI."
+  exec /init
+elif [ -z "$1" ]; then
+  if [ ! -f "/etc/openxpki/.initiated" ]; then
+    echo "================================================"
+    echo "No parameters given and /etc/openxpki/.initiated does not exist."
+    echo "Initiating database and creating configs."
+    echo "================================================"
+    checkDbVariables
+    waitForDbConnection
+    init_db
+    create_config
+    exec /init
+  else
+    echo "================================================"
+    echo "No parameters given and /etc/openxpki/.initiated exist."
+    echo "Starting S6 supervisor"
+    echo "================================================"
+    exec /init
+  fi
+elif [ "$1" == "noinit" ]; then
+  shift
+  exec "$@"
+fi
+else
+  echo "================================================"
+  echo "Starting S6 supervisor with parameters: $@"
+  echo "================================================"
+  exec /init "$@"
+fi
