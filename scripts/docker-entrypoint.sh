997c70de825831f6d48468cf5502ffff55e730ad
Dominic S <git@dominic86.de>
2017-06-01 15:01:06 +0200

    creating database instead of relying on mysql container to create it correctly on startup

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -37,6 +37,26 @@ function waitForDbConnection {
 
 }
 
+function waitForRootDbConnection {
+
+  if [ -z "${APP_DB_ROOT_PASS}" ]; then
+    echo "No root password set, cannot wait for root db connection"
+    exit 1
+  fi
+
+  connectionAttempt=1
+  while ! echo "show databases;" | mysql -u root -p${APP_DB_ROOT_PASS} -h ${APP_DB_HOST} -P ${APP_DB_PORT}; do
+    if [ $connectionAttempt -gt 30 ]; then
+      echo "Maximum amounts of attempts reached, stopping now."; exit 1
+    fi
+    echo "Connection to MySql server failed, waiting 5s before trying again. [${connectionAttempt}/30]"
+    sleep 5
+    connectionAttempt=$[$connectionAttempt +1]
+  done
+  echo "Connection to MySql server was successful."
+
+}
+
 function create_config {
 
   # Use custom configuration if available - otherwise the default
@@ -55,6 +75,26 @@ function create_config {
   
 }
 
+function create_db {
+
+  if [ -z "${APP_DB_ROOT_PASS}" ]; then
+    echo "No root password set, cannot create new database"
+    exit 1
+  fi
+
+  APP_DB_NAME=${APP_DB_NAME:-openxpki}
+  APP_DB_USER=${APP_DB_USER:-openxpki}
+  APP_DB_PASS=${APP_DB_PASS:-openxpki}
+ 
+  echo "CREATE DATABASE ${APP_DB_NAME} CHARSET utf8;" > /tmp/create_db.sh
+  echo "CREATE USER '${APP_DB_USER}'@'%' IDENTIFIED BY '${APP_DB_PASS}';" >> /tmp/create_db.sh
+  echo "GRANT ALL ON ${APP_DB_NAME}.* TO '${APP_DB_NAME}'@'%';" >> /tmp/create_db.sh
+  echo "flush privileges;" >> /tmp/create_db.sh
+
+  cat /tmp/create_db.sh | mysql -u root -p${APP_DB_ROOT_PASS} -h ${APP_DB_HOST} -P ${APP_DB_PORT}
+
+}
+
 function init_db {
 
   # Extract sql file and install database shema
@@ -62,6 +102,7 @@ function init_db {
     mysql -u ${APP_DB_USER} -p${APP_DB_PASS} -D ${APP_DB_NAME} -h ${APP_DB_HOST} -P ${APP_DB_PORT}
 
 }
+
 
 # Check for linked MYSQL container
 if [ -n "${MYSQL_NAME}" ]; then
@@ -99,7 +140,14 @@ if [ -n "${APP_DB_PASS}" ]; then echo "Replacing DB_PASS with given APP_DB_PASS:
 fixPermissions
 
 # Start depending on parameters
-if [ "$1" == "initdb" ]; then
+if [ "$1" == "create_db" ]; then
+  echo "================================================"
+  echo "Received createdb parameter, creating database."
+  echo "================================================"
+  checkDbVariables
+  waitForRootDbConnection
+  create_db
+elif [ "$1" == "init_db" ]; then
   echo "================================================"
   echo "Received initdb parameter, initiating database."
   echo "================================================"
@@ -128,6 +176,10 @@ elif [ -z "$1" ]; then
     echo "Waiting for DB connection before initiating database and creating configs."
     echo "================================================"
     checkDbVariables
+    if [ -n "${APP_DB_ROOT_PASS}" ]; then
+      waitForRootDbConnection
+      create_db
+    fi
     waitForDbConnection
     echo >/etc/openxpki/.initiated
     echo "================================================"
